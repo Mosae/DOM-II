5ec736c51fab2dcf90def56b50d79fa45e367097
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-10 18:19:40 -0500

    Added first event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+//Mouse over
+const busImg = document.querySelector('img');
+const links = document.querySelector('nav')
+console.log(links)
+
+busImg.addEventListener('mouseover', () => {
+    busImg.style.transform = "rotate(" + 90 + "deg)";
+    busImg.style.border = "7px dotted yellow";
+});
+
+busImg.addEventListener('dblclick', () => {
+    busImg.style.transform = "rotate(" + 80 + "deg)";
+    busImg.style.border = "7px dotted yellow";
+});
