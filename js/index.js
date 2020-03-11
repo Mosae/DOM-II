45d1da43c08f95802cef70e3f4db7412e181589c
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-11 11:00:36 -0500

    Added 4 event listeners

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
 // Your code goes here
 //Mouse over
 const busImg = document.querySelector('img');
-const links = document.querySelector('nav')
-console.log(links)
+const links = document.querySelector('nav');
+const letsGo = document.querySelector('.intro');
+const bodycolor = document.querySelector('body')
+
 
 busImg.addEventListener('mouseover', () => {
     busImg.style.transform = "rotate(" + 90 + "deg)";
     busImg.style.border = "7px dotted yellow";
 });
 
-busImg.addEventListener('dblclick', () => {
-    busImg.style.transform = "rotate(" + 80 + "deg)";
-    busImg.style.border = "7px dotted yellow";
+letsGo.addEventListener('mouseenter', () => {
+    letsGo.style.transform = "scale(1.2)";
+    letsGo.style.transition = "all 0.3s";
 });
+
+bodycolor.addEventListener('click', () => {
+    bodycolor.style.color = 'red'
+})
