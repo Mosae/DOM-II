b012e50de65fbe3ddd61fa449d26088fc3da6e77
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-11 13:20:10 -0500

    Added 10 event listeners

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
 // Your code goes here
 //Mouse over
 const busImg = document.querySelector('img');
-const links = document.querySelector('nav');
+const links = document.querySelector('.nav');
 const letsGo = document.querySelector('.intro');
 const bodycolor = document.querySelector('body');
 const button1 = document.querySelector('.btn');
 const dest = document.querySelector('.destination');
-const footer = document.querySelector('.footer');
+const footer = document.querySelector('.footer p');
 const html = document.querySelector('html');
+const h4 = document.querySelector('h4');
+const logo = document.querySelector('.logo-heading');
 
 
 busImg.addEventListener('mouseover', () => {
     busImg.style.transform = "rotate(" + 90 + "deg)";
     busImg.style.border = "7px dotted yellow";
+});
+logo.addEventListener('dblclick', () => {
+    logo.style.transform = "rotate(" + 40 + "deg)";
+    logo.style.border = "2px dotted blue";
+});
+links.addEventListener('mouseenter', () => {
+    links.style.transform = "scale(1.5)";
+    links.style.transition = "all 0.5s";
+    links.style.background = "gray"
 });
 
 letsGo.addEventListener('mouseenter', () => {
@@ -35,4 +46,7 @@ footer.addEventListener('dblclick', () => {
 });
 html.addEventListener('scroll', () => {
     html.style.background = 'red';
+});
+h4.addEventListener('click', () => {
+    h4.style.color = 'blue';
 });
